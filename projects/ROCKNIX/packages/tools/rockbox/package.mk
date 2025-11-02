5d1549b9fa8d40979922db8bd249930ebe95325a
spycat88 <spycat88@users.noreply.github.com>
2025-11-02 23:27:06 +0000

    rockbox: bump version

--- projects/ROCKNIX/packages/tools/rockbox/package.mk
+++ projects/ROCKNIX/packages/tools/rockbox/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rockbox"
-PKG_VERSION="f303f2e06f4da751d9c4543685e7a70ca204e114"
+PKG_VERSION="89b33d233ba6125762c8f552eb2c9a1d57d5d9ce"
 PKG_LICENSE="Proprietary"
 PKG_SITE="https://rocknix.org"
 PKG_URL="https://github.com/ROCKNIX/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
