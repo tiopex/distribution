65ffdbb6db715264ceee9eec0819566c7e13a306
John Williams <porschemad911@gmail.com>
2025-11-07 11:38:34 +1100

    RK3588 - switch from fileman to commander

--- projects/ROCKNIX/packages/misc/modules/package.mk
+++ projects/ROCKNIX/packages/misc/modules/package.mk
@@ -18,7 +18,7 @@ esac
 
 # Fileman or Commander Filemanager
 case ${DEVICE} in
-  SM8250|SDM845|S922X|SM8550)
+  RK3588|SM8250|SDM845|S922X|SM8550)
     PKG_DEPENDS_TARGET+=" commander"
     FILEMANAGER="commander"
   ;;
