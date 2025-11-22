a2f716e5bbd4bf80c01f18da64ee023007da785c
spycat88 <spycat88@users.noreply.github.com>
2025-11-22 13:33:57 +0000

    wxwidgets: fix package

--- projects/ROCKNIX/packages/graphics/wxwidgets/package.mk
+++ projects/ROCKNIX/packages/graphics/wxwidgets/package.mk
@@ -2,15 +2,12 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="wxwidgets"
-PKG_VERSION="b97aee4"
+PKG_VERSION="b97aee419af105e5c724ead2917e1b04b9abb26a"
 PKG_LICENSE="wxWindows Library Licence"
 PKG_SITE="https://github.com/wxWidgets/wxWidgets"
 PKG_URL="${PKG_SITE}.git"
-PKG_GIT_CLONE_BRANCH="3.2"
 PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo gdk-pixbuf gtk3 libaio"
 PKG_LONGDESC="wxWidgets is a free and open source cross-platform C++ framework for writing advanced GUI applications using native controls."
 
-pre_configure_target(){
-  PKG_CMAKE_OPTS_TARGET+=" -DCMAKE_SYSROOT=${SYSROOT_PREFIX}"
-
-}
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_SYSROOT=${SYSROOT_PREFIX} \
+                       -DCMAKE_POLICY_VERSION_MINIMUM=3.5"
