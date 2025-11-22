d177bfcdd9e577e9bc602073e99af77a91996a36
spycat88 <spycat88@users.noreply.github.com>
2025-11-22 23:47:05 +0000

    revert: cemu/wxwidgets changes

--- projects/ROCKNIX/packages/graphics/wxwidgets/package.mk
+++ projects/ROCKNIX/packages/graphics/wxwidgets/package.mk
@@ -2,11 +2,15 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="wxwidgets"
-PKG_VERSION="dac1996ebe162386071c0b9148079fabf3a950e3"
+PKG_VERSION="b97aee4"
 PKG_LICENSE="wxWindows Library Licence"
 PKG_SITE="https://github.com/wxWidgets/wxWidgets"
 PKG_URL="${PKG_SITE}.git"
+PKG_GIT_CLONE_BRANCH="3.2"
 PKG_DEPENDS_TARGET="toolchain zlib libpng libjpeg-turbo gdk-pixbuf gtk3 libaio"
 PKG_LONGDESC="wxWidgets is a free and open source cross-platform C++ framework for writing advanced GUI applications using native controls."
 
-PKG_CMAKE_OPTS_TARGET="-DCMAKE_SYSROOT=${SYSROOT_PREFIX}"
+pre_configure_target(){
+  PKG_CMAKE_OPTS_TARGET+=" -DCMAKE_SYSROOT=${SYSROOT_PREFIX}"
+
+}
