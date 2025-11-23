1bb9cc22b9f673f26ba40b1b9212d4df761eb428
John Williams <porschemad911@gmail.com>
2025-11-24 09:37:55 +1100

    melonds-sa - RK3566 / RK3588 / S922X with libmali needs QT_QPA_PLATFORM=wayland

--- projects/ROCKNIX/packages/emulators/standalone/melonds-sa/scripts/start_melonds.sh
+++ projects/ROCKNIX/packages/emulators/standalone/melonds-sa/scripts/start_melonds.sh
@@ -135,8 +135,16 @@ else
     ROM="${1}"
 fi
 
-#Set QT Platform to Wayland
+# QT platform - default to xcb
 export QT_QPA_PLATFORM=xcb
+
+# QT platform - some device / driver combinations need wayland
+case ${HW_DEVICE} in
+    RK3566|RK3588|S922X)
+        [[ $(/usr/bin/gpudriver) == "libmali" ]] && export QT_QPA_PLATFORM=wayland
+    ;;
+esac
+
 @PANFROST@
 @HOTKEY@
 @LIBMALI@
