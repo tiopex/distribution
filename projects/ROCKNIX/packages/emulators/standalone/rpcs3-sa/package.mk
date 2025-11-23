be3e5683e8fa08ed6d3b5d5945b59fca32d10f6c
Philippe Simons <simons.philippe@gmail.com>
2025-11-23 23:25:36 +0100

    rpcs3: update
    bump to 0.0.38-18386-89a13b75

--- projects/ROCKNIX/packages/emulators/standalone/rpcs3-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/rpcs3-sa/package.mk
@@ -7,8 +7,8 @@ PKG_SITE="https://github.com/RPCS3/rpcs3-binaries-linux"
 PKG_DEPENDS_TARGET="toolchain libevdev SDL2 qt6 mesa libcom-err"
 PKG_LONGDESC="PS3 Emulator appimage"
 PKG_TOOLCHAIN="manual"
-PKG_VERSION="b90bacba4870534dfc501a51119b5913337a5e95"
-PKG_REL_VERSION="0.0.37-18087-b90bacba"
+PKG_VERSION="89a13b75f70815f5018c1168b3f5b80ff49d0005"
+PKG_REL_VERSION="0.0.38-18386-89a13b75"
 
 case ${TARGET_ARCH} in
   x86_64)
