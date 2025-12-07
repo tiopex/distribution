b092085e9a2bd20b7d48d8eb2c9f6d52c2cddca4
Philippe Simons <simons.philippe@gmail.com>
2025-12-07 14:49:29 +0100

    sm8550: drop u-boot

--- projects/ROCKNIX/devices/SM8550/bootloader/update.sh
+++ projects/ROCKNIX/devices/SM8550/bootloader/update.sh
@@ -48,24 +48,6 @@ if [ -f "$SYSTEM_ROOT/usr/share/bootloader/boot/grub/grubenv" ]; then
   fi
 fi
 
-if [ -f "$SYSTEM_ROOT/usr/share/bootloader/rocknix-u-boot.img" ]; then
-  echo "Updating rocknix-u-boot.img..."
-  cp $SYSTEM_ROOT/usr/share/bootloader/rocknix-u-boot.img $BOOT_ROOT
-fi
-
-if [ -d "$SYSTEM_ROOT/usr/share/bootloader/device_trees" ]; then
-  mkdir -p $BOOT_ROOT/device_trees
-  echo "Updating u-boot dtbs..."
-  cp $SYSTEM_ROOT/usr/share/bootloader/device_trees/*.dtb $BOOT_ROOT/device_trees
-fi
-
-U_BOOT_DT_ID=$(cat /proc/device-tree/rocknix-u-boot-dt-id)
-UPDATE_DTB_SOURCE="$BOOT_ROOT/device_trees/$U_BOOT_DT_ID.dtb"
-if [ -f "$UPDATE_DTB_SOURCE" ]; then
-  echo "Updating dtb.img from $(basename $UPDATE_DTB_SOURCE)..."
-  cp "$UPDATE_DTB_SOURCE" "$BOOT_ROOT/dtb.img"
-fi
-
 if [ -d "$SYSTEM_ROOT/usr/share/bootloader/rocknix_abl" ]; then
   mkdir -p $BOOT_ROOT/rocknix_abl
   echo "Updating ROCKNIX ABL..."
