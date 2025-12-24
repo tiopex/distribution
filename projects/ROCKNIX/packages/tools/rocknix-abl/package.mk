5c548e26f45af4d99fea80ed5de0d19df30f07ef
spycat88 <spycat88@users.noreply.github.com>
2025-12-24 09:35:15 +0000

    rocknix-abl: update package

--- projects/ROCKNIX/packages/tools/rocknix-abl/package.mk
+++ projects/ROCKNIX/packages/tools/rocknix-abl/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rocknix-abl"
-PKG_VERSION="bbc794d609bdd4d80a5076f68cc4edc5bcd7df36"
+PKG_VERSION="894f0fc42fc2531e06a24be92cc70934095aec76"
 PKG_ARCH="aarch64"
 PKG_SITE="https://github.com/ROCKNIX/abl"
 PKG_URL="https://github.com/ROCKNIX/abl/archive/${PKG_VERSION}.tar.gz"
