1957458649ac6fdd3edd3185752482e3e1f56573
spycat88 <spycat88@users.noreply.github.com>
2026-01-01 17:55:36 +0000

    rocknix-abl: bump package

--- projects/ROCKNIX/packages/tools/rocknix-abl/package.mk
+++ projects/ROCKNIX/packages/tools/rocknix-abl/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="rocknix-abl"
-PKG_VERSION="59226e5a7ad524b5cfbbdf1d123f815069d2a309"
+PKG_VERSION="d0778f09897c94ba7363c49aa9a28d9614653689"
 PKG_ARCH="aarch64"
 PKG_SITE="https://github.com/ROCKNIX/abl"
 PKG_URL="https://github.com/ROCKNIX/abl/archive/${PKG_VERSION}.tar.gz"
