779e6d14cd8e402a64d94c6b9ae3772f4014b254
tiopex <tiopxyz@gmail.com>
2026-02-23 15:37:38 +0100

    bump box64

--- projects/ROCKNIX/packages/compat/box64/package.mk
+++ projects/ROCKNIX/packages/compat/box64/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="box64"
-PKG_VERSION="3ec5de03c786333ed8d5a51c5b35a8bd6e22b229"
+PKG_VERSION="c7bc2932925879f5758c5c2185460c823f97ceab"
 PKG_ARCH="aarch64"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/ptitSeb/box64"
