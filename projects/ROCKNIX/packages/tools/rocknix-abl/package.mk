a59ca392284bb2461df0f9552510af376191b3d5
tiopex <tiopxyz@gmail.com>
2026-03-23 08:30:24 +0100

    sm8250 test

--- projects/ROCKNIX/packages/tools/rocknix-abl/package.mk
+++ projects/ROCKNIX/packages/tools/rocknix-abl/package.mk
@@ -11,7 +11,8 @@ PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
   mkdir -p ${INSTALL}/usr/share/bootloader/rocknix_abl
-    cp ${PKG_BUILD}/abl_signed-${DEVICE}.elf ${INSTALL}/usr/share/bootloader/rocknix_abl/abl_signed-${DEVICE}.elf
+    cp ${PKG_BUILD}/abl_signed-SM8550.elf ${INSTALL}/usr/share/bootloader/rocknix_abl/abl_signed-${DEVICE}.elf
+    cp ${PKG_BUILD}/abl_signed-SM8550.elf ${INSTALL}/usr/share/bootloader/rocknix_abl/abl_signed-SM8550.elf
     cp ${PKG_DIR}/sources/* ${INSTALL}/usr/share/bootloader/rocknix_abl
     mv ${INSTALL}/usr/share/bootloader/rocknix_abl/flash_abl.sh.template ${INSTALL}/usr/share/bootloader/rocknix_abl/flash_abl.sh
     sed -i "s/%DEVICE%/${DEVICE}/g" ${INSTALL}/usr/share/bootloader/rocknix_abl/flash_abl.sh
